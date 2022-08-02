70fe83d212d4d69653b269b2c4b5d8c175db7621
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-02 09:43:46 +0900

    ユーザー情報　削除

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,36 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("users router");
+// ユーザー更新
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("ユーザー情報が更新されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("ご自身のアカウントのみ更新可能です");
+  }
 });
+
+// ユーザー削除
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザー情報が削除されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("ご自身のアカウントのみ削除可能です");
+  }
+});
+
+// ユーザー取得
 
 module.exports = router;
