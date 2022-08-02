293a4b2649d44fae5508f0a9cc058ea26c0ea7bc
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-02 17:58:33 +0900

    Post投稿、更新機能作成

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,32 @@
-const { route } = require("./users");
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-// router.get("/", (req, res) => {
-//   res.send("posts router");
-// });
+// 投稿作成
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// 投稿を更新する
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿編集に成功しました！");
+    } else {
+      return res.status(403).json("他の人の投稿を編集できません");
+    }
+  } catch (err) {
+    return res.status(403).json(err);
+  }
+});
 
 module.exports = router;
