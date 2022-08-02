a70d45a540ebee04a408caeb49768257704d30f3
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-02 15:54:43 +0900

    フォロー機能作成

--- routes/users.js
+++ routes/users.js
@@ -42,4 +42,36 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      // フォロワーに自分が含まれていなかったらフォローできる
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json("フォローに成功しました。");
+      } else {
+        return res
+          .status(403)
+          .json("あなたは既にこのユーザーをフォローしています。");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json("自分自身はフォローできません。");
+  }
+});
+
 module.exports = router;
