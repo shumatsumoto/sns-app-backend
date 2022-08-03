e1f357d834e04c61677be078ef3cdfe60f9a59b7
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-03 11:41:07 +0900

    タイムライン取得

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 // 投稿作成
 router.post("/", async (req, res) => {
@@ -75,6 +76,23 @@ router.put("/:id/like", async (req, res) => {
       });
       return res.status(403).json("投稿からいいねを外しました");
     }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// タイムラインの投稿を取得
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    // 自分がフォローしている友達の投稿内容を全て取得する。
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    return res.status(200).json(userPosts.concat(...friendPosts));
   } catch (err) {
     return res.status(500).json(err);
   }
