7c3e9bf984f81fb0648e279d3f85a632842a0a74
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-15 15:35:45 +0900

    プロフィール専用のタイムライン取得

--- routes/posts.js
+++ routes/posts.js
@@ -81,6 +81,17 @@ router.put("/:id/like", async (req, res) => {
   }
 });
 
+// プロフィール専用のタイムライン取得
+router.get("/profile/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    const posts = await Post.find({ userId: user._id });
+    return res.status(200).json(posts);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 // タイムラインの投稿を取得
 router.get("/timeline/:userId", async (req, res) => {
   try {
