9d1d264dbd7892c06a1e3260b098d3f1fad1a256
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-15 16:11:59 +0900

    クエリを使ってユーザー情報取得

--- routes/users.js
+++ routes/users.js
@@ -32,9 +32,24 @@ router.delete("/:id", async (req, res) => {
 });
 
 // ユーザー取得
-router.get("/:id", async (req, res) => {
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id);
+//     const { password, updatedAt, ...other } = user._doc;
+//     return res.status(200).json(other);
+//   } catch (err) {
+//     return res.status(500).json(err);
+//   }
+// });
+
+// クエリでユーザー情報を取得
+router.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
   try {
-    const user = await User.findById(req.params.id);
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
     const { password, updatedAt, ...other } = user._doc;
     return res.status(200).json(other);
   } catch (err) {
