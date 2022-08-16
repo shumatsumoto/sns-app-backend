7ba2cc3ba153034449324eefcd33346fd3dca5fd
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-16 09:09:35 +0900

    img upload 設定

--- routes/upload.js
+++ routes/upload.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 const multer = require("multer");
 
+// 画像アップロード
+const upload = multer();
+router.post("/", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("画像アップロードに成功しました！");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
