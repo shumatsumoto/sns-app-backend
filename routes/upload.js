1ab7ad53118f9eb71ed7b9f91b66c366de806dd4
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-16 09:12:44 +0900

    画像アップロードAPI

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,15 @@ const router = require("express").Router();
 const multer = require("multer");
 
 // 画像アップロード
-const upload = multer();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 router.post("/", upload.single("file"), (req, res) => {
   try {
     return res.status(200).json("画像アップロードに成功しました！");
